56be15fac3f247cba17430b97af2770462fc2a66
Woody <waiyanwoody@gmail.com>
2025-04-26 00:20:50 +0630

    auth update

--- app/chats/layout.tsx
+++ app/chats/layout.tsx
@@ -1,11 +1,5 @@
+import AuthenticatedLayout from "@/components/layout/AuthenticatedLayout"
 import type React from "react"
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { Separator } from "@/components/ui/separator"
-import AuthenticatedLayout from "../AuthenticatedLayout"
-import { AppSidebar, navigationItems } from "@/components/layout/app-sidebar"
-import { MobileNavigation } from "@/components/layout/mobile-navigation"
-import { ThemeToggle } from "@/components/theme-toggle"
-import { UserDropdown } from "@/components/user-dropdown"
 
 export default function DashboardLayout({
     children,
