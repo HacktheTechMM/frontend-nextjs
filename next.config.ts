8dacf5836ac4f131b846b9749c11cd540e6f27a8
Woody <waiyanwoody@gmail.com>
2025-04-25 19:55:46 +0630

    try to deploy

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     domains: ['res.cloudinary.com'],
   },
-  darkMode: 'class',  
   eslint: {
     ignoreDuringBuilds: true
   },
