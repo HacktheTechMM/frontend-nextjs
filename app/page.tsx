20b6cc7ba359231327452b1b0baafdd17b71382d
Woody <waiyanwoody@gmail.com>
2025-04-27 11:00:09 +0630

    hero section update

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,19 @@ import FooterSection from "@/components/footer";
 import HeroSection from "@/components/hero-section";
 import { HeroHeader } from "@/components/hero5-header";
 import IntegrationsSection from "@/components/integrations-5";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('USER');
+    if (storedUser) {
+      router.push('/chats');
+    }
+  }, []);
 
   return (
     <>
